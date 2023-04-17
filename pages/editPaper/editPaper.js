1956ef1eb68327cd698d8746e31d3647262327e2
YeZuYn <1329591080@qq.com>
2023-04-17 14:51:58 +0800

    feat: 添加编辑题型 popup 组件 feat: 添加单选题型

--- pages/editPaper/editPaper.js
+++ pages/editPaper/editPaper.js
@@ -5,8 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
+    addPopupVisible: false,
+    questionList: [],
+    baseList: [
+      { id: "1", icon: "", text: "单选题", url: "../../components/radio/radio", },
+      { id: "2", icon: "", text: "多选题", url: "", },
+      { id: "3", icon: "", text: "填空题", url: "", },
+      { id: "4", icon: "", text: "图片上传", url: "", },
+    ],
+    templateList: [
+      { id: "1", icon: "", text: "姓名", url: "", },
+      { id: "2", icon: "", text: "性别", url: "", },
+      { id: "3", icon: "", text: "手机", url: "", },
+      { id: "4", icon: "", text: "日期", url: "", },
+      { id: "5", icon: "", text: "地区", url: "", },
+    ],
+    batchList: [
+      { id: "1", icon: "", text: "题库选题", url: "", },
+      { id: "2", icon: "", text: "文本导入", url: "", },
+    ]
+  }, 
 
+  // tab change
+  onChange(event) {
+    if (event.detail) {
+      switch (event.detail) {
+        case "add": 
+          this.setData({
+            addPopupVisible: true,
+          });
+          break;
+      }
+    }
+    
   },
+
+  // 弹出层关闭
+  onPopupClose() {
+    this.setData({
+      addPopupVisible: false,
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
